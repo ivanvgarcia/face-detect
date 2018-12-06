6239c02fd3de03a705c5406f0ca87185e482de12
Ivan Garcia <ivanvlora@gmail.com>
2018-12-06 09:28:49 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import "./App.css";
+import Particles from "react-particles-js";
 
 class App extends Component {
   render() {
+    const particleOptions = {
+      particles: {
+        number: {
+          value: 200,
+          density: {
+            enable: false
+          }
+        },
+        size: {
+          value: 10,
+          random: true,
+          anim: {
+            speed: 4,
+            size_min: 0.3
+          }
+        },
+        line_linked: {
+          enable: false
+        },
+        move: {
+          random: true,
+          speed: 1,
+          direction: "top",
+          out_mode: "out"
+        }
+      },
+      interactivity: {
+        events: {
+          onhover: {
+            enable: true,
+            mode: "bubble"
+          },
+          onclick: {
+            enable: true,
+            mode: "repulse"
+          }
+        },
+        modes: {
+          bubble: {
+            distance: 250,
+            duration: 2,
+            size: 0,
+            opacity: 0
+          },
+          repulse: {
+            distance: 400,
+            duration: 4
+          }
+        }
+      }
+    };
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Particles className="particles" params={particleOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
       </div>
     );
   }
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Tilt from "react-tilt";
+import faceDetect from "./face-detection.png";
+import "./Logo.css";
+
+const Logo = () => {
+  return (
+    <div className="ma4 ">
+      <Tilt
+        className="Tilt br2 shadow-2"
+        options={{ max: 55 }}
+        style={{ height: 250, width: 250 }}
+      >
+        <div className="Tilt-inner">
+          {" "}
+          <h1>Face Detect</h1>
+          <img src={faceDetect} alt="face detect icon" />{" "}
+        </div>
+      </Tilt>
+    </div>
+  );
+};
+
+export default Logo;
